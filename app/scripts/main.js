2ae7102f5f8dd4faaf1976b1fa300e84f7a330c5
Josh <joshisadesigner@gmail.com>
2018-01-10 17:45:43 -0600

    Initialize cat clicker premium requirements

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,22 +1,40 @@
-const triggerClass = 'clicked';
+
+const imageOne = document.getElementById( 'targetImageOne' );
+const imageTwo = document.getElementById( 'targetImageTwo' );
+const clickCounterOne = document.getElementById( 'clickCounterOne' );
+const clickCounterTwo = document.getElementById( 'clickCounterTwo' );
+let clicksImageOne = 0;
+let clicksImageTwo = 0;
+
 
 const imageTrigger = {
-    addClass : ( elementTarget ) => elementTarget.className += ` ${triggerClass}`,
-    removeClass : ( elementTarget ) => elementTarget.classList.remove( triggerClass ),
-    clickAction : ( element ) => {
-        let clicks = 0;
-        clicks += 1;
-        console.log( 'clickCounter' );
-        element.classList.remove( triggerClass )
-    },
-    catchMouseEvent : () => {
-        const image = document.getElementById( 'targetImage' );
-        const clickCounter = document.getElementById( 'clickCounter' );
+    catchMouseEvent : ( image, counter, int ) => {
+        const triggerClass = 'clicked';
         if ( image.addEventListener ){
             image.addEventListener( 'mousedown', () => image.className += ` ${triggerClass}`, false );
-            image.addEventListener( 'mouseup', imageTrigger.clickAction( image ), false);
+            image.addEventListener( 'mouseup', () => {
+                image.classList.remove( triggerClass );
+                counter.textContent = int += 1;
+            }, false);
         }
+    },
+    catchFormNames : () => {
+        const button = document.getElementById( 'formButton' );
+        const inputOne = document.getElementById( 'inputOne' );
+        const inputTwo = document.getElementById( 'inputTwo' );
+        const backdrop = document.getElementById( 'backdrop' );
+        const firstName = document.getElementById( 'nameOne' );
+        const secondName = document.getElementById( 'nameTwo' );
+        button.addEventListener( 'click', () => {
+            if ( inputOne.value.length > 0 && inputTwo.value.length ) {
+                firstName.textContent = inputOne.value;
+                secondName.textContent = inputTwo.value;
+                backdrop.className += ' fadeOut';
+            }
+        }, false );
     }
 }
 
-imageTrigger.catchMouseEvent();
+imageTrigger.catchMouseEvent( imageOne, clickCounterOne, clicksImageOne );
+imageTrigger.catchMouseEvent( imageTwo, clickCounterTwo, clicksImageTwo );
+imageTrigger.catchFormNames();
