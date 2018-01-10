daf9d7256edb0ea8993ae8a743a2392ad22a3721
Josh <joshisadesigner@gmail.com>
2018-01-09 18:12:31 -0600

    Add click actions on counter

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,20 @@
-
-
 const triggerClass = 'clicked';
 
 const imageTrigger = {
     addClass : ( elementTarget ) => elementTarget.className += ` ${triggerClass}`,
     removeClass : ( elementTarget ) => elementTarget.classList.remove( triggerClass ),
-    whenClick : () => image.addEventListener,
+    clickAction : ( element ) => {
+        let clicks = 0;
+        clicks += 1;
+        console.log( 'clickCounter' );
+        element.classList.remove( triggerClass )
+    },
     catchMouseEvent : () => {
         const image = document.getElementById( 'targetImage' );
+        const clickCounter = document.getElementById( 'clickCounter' );
         if ( image.addEventListener ){
             image.addEventListener( 'mousedown', () => image.className += ` ${triggerClass}`, false );
-            image.addEventListener( 'mouseup', () => image.classList.remove( triggerClass ), false );
+            image.addEventListener( 'mouseup', imageTrigger.clickAction( image ), false);
         }
     }
 }
