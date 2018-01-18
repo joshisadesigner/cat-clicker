a3e1c380e4e2d3587cfcf9ef0677f6445ed17ddd
Josh <joshisadesigner@gmail.com>
2018-01-18 13:21:29 -0600

    Convert html collection to array

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,22 @@
-const catThumbnails = document.getElementsByClassName( 'cat' );
-const catsSelected = [];
 
-catThumbnails.forEach(element => {
-    if ( element.addEventListener ) {
-        
+const thumbs = Array.from( document.getElementsByClassName( 'cat' ) );
+const selectedImages = [];
+
+const imageSelection = {
+    changeIcon: ( object ) => {
+        const unselect = 'far fa-circle';
+        const selected = 'fas fa-check-circle';
+        if( object.className === unselect ) {
+            object.className === selected;
+        } else {
+            object.className === unselect;
+        }
+    },
+    onClick: ( arr ) => {
+        arr.forEach( ( element ) => {
+            element.addEventListener( 'click', () => { console.log( 'element clicked' ) }, false );
+        } );
     }
-});
+}
+
+imageSelection.onClick( thumbs );
