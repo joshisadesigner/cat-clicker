d73ee70bd84c76dd20e8f076b16ff77c87a6ef95
Josh <joshisadesigner@gmail.com>
2018-01-18 15:55:02 -0600

    Build function to change icon on thumbnail selected

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,21 +1,56 @@
 
-const thumbs = Array.from( document.getElementsByClassName( 'cat' ) );
+const thumbs = document.getElementsByClassName( 'cat' );
 const selectedImages = [];
 
 const imageSelection = {
     changeIcon: ( object ) => {
-        const unselect = 'far fa-circle';
-        const selected = 'fas fa-check-circle';
-        if( object.className === unselect ) {
-            object.className === selected;
+        const unselect = 'form__select-icon far fa-circle';
+        const selected = 'form__select-icon fas fa-check-circle';
+
+        console.log( object );
+
+        if( object.classList.contains( 'far' ) ) {
+            object.className = selected;
         } else {
-            object.className === unselect;
+            object.className = unselect;
         }
     },
     onClick: ( arr ) => {
-        arr.forEach( ( element ) => {
-            element.addEventListener( 'click', () => { console.log( 'element clicked' ) }, false );
-        } );
+        for( let i = 0; i < arr.length; i++ ) {
+
+            let element = arr[ i ];
+            
+            arr[ i ].addEventListener( 'click', ( ( elm ) => {
+                let elmIcon = elm.getElementsByClassName( 'far' );
+                return () => {
+                    console.log( elm );
+                };
+            })( element ));
+        }
+    }
+}
+
+const htmlElements = {
+    input : ( identifier ) => {
+        let input = `<div class="form__group">
+                        <img class="form__group-img" src="images/${identifier}.jpg" />
+                        <input id="input_0${identifier}" class="form__input" type="text">
+                    </div>`
+        continer.innerHTML += input;
+    },
+    image : ( identifier ) => {
+        let image = `<div id="targetImage_0${ i }" class="box__unit">
+                        <h3 id="name_0${identifier}" class="box__title cat-name"></h3>
+                        <img id="image-00" class="box__img" src="images/${identifier}.jpg" />
+                        <span id="counter_0${identifier}" class="box-counter">0</span>
+                    </div>`
+        container.innerHTML += image;
+    },
+    insertHtmlElement : ( container, element ) => {
+        container.innerHTML += element;
+    },
+    emptyHtmlContainer : ( container ) => {
+        container.innerHTML = '';
     }
 }
 
