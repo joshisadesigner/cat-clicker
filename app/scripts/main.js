e31028d91c55bc4cb48ee1771a90ca704f8cadc9
Josh <joshisadesigner@gmail.com>
2018-05-04 17:40:56 -0600

    Build up the view

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
   const model = [
     {
       name: 'Ashes',
@@ -27,9 +27,28 @@
 
     populateList: () => {},
 
-    setCurrentCat: () => {},
+    setCurrentCat: () => {
+    },
 
-    clickOnCat: () => {},
+    clickOnCatList: () => {
+      let catList = document.querySelectorAll('btn');
+
+      for (let i = 0; i < catList.length; i++) {
+        const cat = catList[i];
+
+        cat.addEventLitener('click', ((catCopy) => {
+          let catObject = {
+            name: model[i].name,
+            clicks: model[i].clicks,
+            index: i,
+          }
+          return () => {
+            catObject
+          }
+        })(cat));
+        
+      }
+    },
 
     init: () => {
       view.init();
@@ -37,27 +56,36 @@
   };
 
   const view = {
-
     init: () => {
       view.renderListOfCats();
     },
 
     renderListOfCats: () => {
       const catButtons = document.getElementById('catButtons');
-      let htmlStr = '';
+      let catButtonStr = '';
       for (let i = 0; i < model.length; i++) {
         const element = model[i];
-        htmlStr +=
-          `<button id="cat-0${i}" class="btn selection__btn">
+        catButtonStr += 
+        `<button id="cat-0${i}" class="btn selection__btn">
           <img src="images/${i}.jpg" class="btn__image" alt=""> ${model[i].name}
-          </button>`
+        </button>`;
       }
-      catButtons.innerHTML = htmlStr;
+      catButtons.innerHTML = catButtonStr;
     },
 
-    renderCatViewingArea: () => {},
+    renderCatViewingArea: () => {
+      const catView = document.getElementById('catView');
+      let catActive = octopus.setCurrentCat();
+      let catStr = '';
+      catView.innerHTML = 
+      `<div id="box" class="box">
+        <img src="images/${catActive.index}.jpg" alt="Cat #${catActive.index}" id="box-img" class="img box__img">
+        <span id="box-title" class="box__element_float box__element_title">${catActive.name}</span>
+        <span id="box-counter" class="box__element_float box__element_counter">${catActive.clicks}</span>
+      </div>`;
+    },
 
-    render: () => {},
+    render: () => {}
   };
 
   octopus.init();
